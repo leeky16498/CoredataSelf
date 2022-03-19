f212f6a9380bb8aaf732f8310c66e90583251807
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-19 11:57:16 +0000

    Initial Commit

---
+++ CoredataSelf/CoredataSelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoredataSelfApp.swift
+//  CoredataSelf
+//
+//  Created by Kyungyun Lee on 19/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoredataSelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
